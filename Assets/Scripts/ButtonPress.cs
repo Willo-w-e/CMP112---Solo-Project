5b14d1a874a0d72984d53f9401e10f1886aa6fdb
Willo-w-e <2401256@uad.ac.uk>
2025-11-15 17:54:19 +0000

    Added framework for button script

--- Assets/Scripts/ButtonPress.cs
+++ Assets/Scripts/ButtonPress.cs
@@ -9,26 +9,51 @@ public class ButtonPress : MonoBehaviour
 {
     public object target;
     public int duration;
-
+    private int countdown;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (countdown > 0)
+        {
+            countdown--;
+
+            if (countdown == 0)
+            {
+                DisableTarget();
+            }
+        }
     }
 
 
     private void OnCollisionEnter(Collision other)
     {
-        if(duration == 0)
+        if (duration == 0)
         {
-            
+            ActivateTarget();
         }
+
+        else
+        {
+            ActivateTarget();
+            countdown = duration;
+        }
+    }
+
+    private void ActivateTarget()
+    {
+
+    }
+
+    private void DisableTarget()
+    {
+
     }
 }
