99b710d17a78b1c62149d9059671ad69ee916b90
Willo-w-e <2401256@uad.ac.uk>
2025-12-08 00:38:51 +0000

    More comments
    See previous commit

--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -21,19 +21,19 @@ public class PlayerRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 mouse = lookDelta * mouseSensitivity * Time.deltaTime;
+        Vector2 mouse = lookDelta * mouseSensitivity * Time.deltaTime; //Creates a readable vector of the mouses movement delta
 
-        playerrotation.Rotate(Vector3.up * mouse.x);
+        playerrotation.Rotate(Vector3.up * mouse.x); //Rotates the player based off the x of the mouses movement delta
 
-        Rotation -= mouse.y;
-        Rotation = Mathf.Clamp(Rotation, -90f, 90f);
+        Rotation -= mouse.y; //Sets rotation to the mouses movement delta on the Y axis
+        Rotation = Mathf.Clamp(Rotation, -90f, 90f); //Prevents the camera from breaking your neck, cause that would be BAD by clamping the angle between 90 and -90
 
-        camerarotation.localRotation = Quaternion.Euler(Rotation, 0f, 0f);
+        camerarotation.localRotation = Quaternion.Euler(Rotation, 0f, 0f); //Does the rotation on the camera
     }
 
     public void OnRotation(InputValue value)
     {
-        lookDelta = value.Get<Vector2>();
+        lookDelta = value.Get<Vector2>(); //Updates lookdelta to current mouse location
     }
 
 
